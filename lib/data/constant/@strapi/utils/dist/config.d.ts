22008c1aa0c4a471f2e00078b2046d5ee1005a55
Kushalgurung23 <qoosal23.kg@gmail.com>
2023-07-10 17:39:48 +0800

    created database schema and inserted data in strapi server.

--- lib/data/constant/@strapi/utils/dist/config.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from './types';
-export declare const getConfigUrls: (config: Config, forAdminBuild?: boolean) => {
-    serverUrl: string;
-    adminUrl: any;
-    adminPath: any;
-};
-export declare const getAbsoluteAdminUrl: (config: Config, forAdminBuild?: boolean) => any;
-export declare const getAbsoluteServerUrl: (config: Config, forAdminBuild?: boolean) => any;
